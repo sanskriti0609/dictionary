b1242ba2ce202b60858bc224594de8457b385d24
sanskriti0609 <sanskritisingh6350@gmail.com>
2021-07-26 20:08:22 +0530

    commit file

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+
+dic = { 'enjoyed':'got joy and pleasure from',
+                         'wonderful' : 'very good  amazing',
+                         'thinning' : 'making or becoming',
+                        'delighted ': 'very  pleasure',
+                         'company': 'a friend  or family'}
+words =input("enter a word please" )
+print(dic[input()])
